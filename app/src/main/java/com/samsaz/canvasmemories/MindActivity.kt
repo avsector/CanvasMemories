830dd65aa28e6d66dc368be75cfe5ffaf028e047
Hamid Moazzami <mblcdr@gmail.com>
2018-12-10 18:22:32 +0330

    Initial commit

---
+++ app/src/main/java/com/samsaz/canvasmemories/MindActivity.kt
@@ -0,0 +1,17 @@
+package com.samsaz.canvasmemories
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Copyright 2018
+ * Created and maintained by Hamid Moazzami
+ */
+
+class MindActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mind)
+    }
+}
